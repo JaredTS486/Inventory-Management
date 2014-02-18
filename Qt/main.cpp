eb7ab4c13670a780f4e5915a7618dcfcf7550570
JaredTS486 <JaredTS486@gmail.com>
2014-02-18 09:31:49 -0700

    Added testing calls for program modules

--- Qt/main.cpp
+++ Qt/main.cpp
@@ -1,12 +1,34 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include "database.h"
+#include "qrgen.h"
+#include "printer.h"
+
+int CallPrinter()
+{
+    Print();
+    return 0;
+}
+
+int CallDatabase()
+{
+    Connect();
+    return 0;
+}
+
+int CallQRGen()
+{
+    QRGen();
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Connect();
     return a.exec();
+    CallPrinter();
+    CallDatabase();
+    CallQRGen();
 }
