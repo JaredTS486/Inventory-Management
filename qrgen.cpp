982bb20d51f2441f7c8f6c16170e3a7675bfbf3b
ChrJones89 <cjones056@gmail.com>
2014-03-11 12:15:34 -0600

    Updated QRGen & added test functionality
    QRGen should now take a string as input, print out that string to the monitor/terminal for reference, and return a QR code. When ran, it should display the QR code in a terminal/console/monitor.

--- qrgen.cpp
+++ qrgen.cpp
@@ -6,22 +6,22 @@
 using namespace std;
 
 
-unsigned char *QRGen(string dataString)
+unsigned char *QRGen(string dataString)//Now takes single string param
 {
-   //string sampleDataString = "It Works!!!";
+   //string sampleDataString = "It Works!!!"; Keep for backup, in case input does not work.
    QRcode *qrc;
    qrc = new QRcode;
    qrc = QRcode_encodeString8bit(dataString.c_str(),4,QR_ECLEVEL_Q);
-   cout << "The QRC data should be " << dataString << endl;
+   cout << "The QRC data string should be " << dataString << endl; //Ensure the input is correct.
    return qrc->data;
 }
 
-void test()
+void test()//function to test QRGen
 {
-   cout << QRGen("Hello, World") << endl;
+   cout << QRGen("Hello, World") << endl;//should print out QRC data from QRGen
 }
 
-int main()
+int main()//used to call test function
 {
 	test();
 	return 0;
