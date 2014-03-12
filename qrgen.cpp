109e607973f1cdb8312e941028dc47e227b2638a
ChrJones89 <cjones056@gmail.com>
2014-03-12 11:02:34 -0600

    Update qrgen.cpp

--- qrgen.cpp
+++ qrgen.cpp
@@ -9,11 +9,22 @@ using namespace std;
 unsigned char *QRGen(string dataString)//Now takes single string param
 {
    //string sampleDataString = "It Works!!!"; Keep for backup, in case input does not work.
-   QRcode *qrc;
-   qrc = new QRcode;
-   qrc = QRcode_encodeString8bit(dataString.c_str(),4,QR_ECLEVEL_Q);
-   cout << "The QRC data string should be " << dataString << endl; //Ensure the input is correct.
-   return qrc->data;
+   if(dataString != NULL || dataString != "") //make sure that the input actually contains data first.
+   {
+   	QRcode *qrc;
+   	qrc = new QRcode;
+   	qrc = QRcode_encodeString8bit(dataString.c_str(),4,QR_ECLEVEL_Q);
+   	cout << "The QRC data string should be " << dataString << endl; //Ensure the input is correct.
+   	return qrc->data;
+   }
+   
+   else //convert -1 to unsigned char* and return it for error;
+   {
+    	unsigned char *error;
+    	error = new unsigned char;
+    	*error = (unsigned char)-1;
+    	return *error;
+   }
 }
 
 void test()//function to test QRGen
