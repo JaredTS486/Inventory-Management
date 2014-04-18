194b6e6181f3835263061c68ff2b7df9117ca427
JaredTS486 <JaredTS486@gmail.com>
2014-04-17 20:19:15 -0600

    Fixed Order of Classes Also MySQL code works now.

--- Visual Studio/Inventory-Management/Main.cs
+++ Visual Studio/Inventory-Management/Main.cs
@@ -14,55 +14,6 @@ using MySql.Data.MySqlClient;
 
 namespace Travel_Management
 {
-    public partial class DBI
-    {
-        private String usr;
-        private String pwd;
-        private String srv;
-        private String dbs;
-        private MySqlConnection conn;
-        public void SetPass(String P) { this.pwd = P; }
-        public void SetUser(String U) { this.usr = U; }
-        public void SetServer(String S) { this.srv = S; }
-        public void SetDatabase(String D) { this.dbs = D; }
-        public String GetUser() { return this.usr; }
-        public String GetPass() { return this.pwd; }
-        public String GetServer() { return this.srv; }
-        public String GetDatabase() { return this.dbs; }
-        public bool TestDB(){
-            string connectionString = "database=" + this.dbs + ";server=" + this.srv + ";uid=" + this.usr + ";pwd=" + this.pwd;
-            using (conn = new MySqlConnection(connectionString)){
-                try{
-                    conn.Open();
-                    return true;
-                }
-                catch (MySqlException ex){
-                    switch (ex.Number){
-                        case 4060:
-                            Console.WriteLine("INVALID DATABASE");
-                            return false;
-                        case 18456:
-                            Console.WriteLine("LOGIN FAILED");
-                            return false;
-                        default:
-                            Console.WriteLine("ERROR: " + ex);
-                            return false;
-                    }
-                }
-            }
-        }
-    }
-    public partial class LabelStuff
-    {
-        private ILabel label;
-        public LabelStuff(String LabelName){label = DYMO.Label.Framework.Label.Open(LabelName);}
-        public LabelStuff(){ }
-        private void SetBarcode (String BarcodeData) {label.SetObjectText("BARCODE", BarcodeData);}
-        private void SetInfoData(String InfoData) {label.SetObjectText("INFOTXT", InfoData);}
-        private String GetBarcode() { return label.GetObjectText("BARCODE"); }
-        private String GetLabel() { return label.GetObjectText("INFOTXT"); }
-        private void PrintLabel() { label.Print("DYMO LabelWriter 400"); }
-    }
     public partial class Main : Form
     {
         private OpenFileDialog OpenFile1;
@@ -76,24 +27,20 @@ namespace Travel_Management
             InitializeComponent();
             dbVars.TestDB();
         }
-
         public void AddDataMethod(String myString)
         {
             //Change form values here
         }
-
         public void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
             SerialPort sp = (SerialPort)sender;
             string indata = sp.ReadExisting();
             Console.Write(indata);
         }
-
         public void Set_InputTestData(String indata)
         {
 
         }
-
         private void Browse_Click(object sender, System.EventArgs e){
             //string str = FileNameEdit.Text;
             //int i = str.LastIndexOf("\\");
@@ -111,32 +58,26 @@ namespace Travel_Management
         {
             this.myDelegate = new AddDataDelegate(AddDataMethod);
         }
-
-
-
         private void Main_Load_1(object sender, EventArgs e)
         {
             receivingPanel.Hide();
             reusePanel.Hide();
         }
-
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
-
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
 
         }
-
         private void menuListbox_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (menuListbox.SelectedItem == "Receiving")
             {
                 reusePanel.Hide();
                 receivingPanel.Show();
-        }
+            }
             if (menuListbox.SelectedItem == "Reuse")
             {
                 receivingPanel.Hide();
@@ -194,5 +135,59 @@ namespace Travel_Management
         {
 
         }
+    }
+    public partial class DBI
+    {
+        private String usr;
+        private String pwd;
+        private String srv;
+        private String dbs;
+        private MySqlConnection conn;
+        public void SetPass(String P) { this.pwd = P; }
+        public void SetUser(String U) { this.usr = U; }
+        public void SetServer(String S) { this.srv = S; }
+        public void SetDatabase(String D) { this.dbs = D; }
+        public String GetUser() { return this.usr; }
+        public String GetPass() { return this.pwd; }
+        public String GetServer() { return this.srv; }
+        public String GetDatabase() { return this.dbs; }
+        public bool TestDB()
+        {
+            string connectionString = "database=" + this.dbs + ";server=" + this.srv + ";uid=" + this.usr + ";pwd=" + this.pwd;
+            using (conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch (MySqlException ex)
+                {
+                    switch (ex.Number)
+                    {
+                        case 4060:
+                            Console.WriteLine("INVALID DATABASE");
+                            return false;
+                        case 18456:
+                            Console.WriteLine("LOGIN FAILED");
+                            return false;
+                        default:
+                            Console.WriteLine("ERROR: " + ex);
+                            return false;
+                    }
+                }
+            }
+        }
+    }
+    public partial class LabelStuff
+    {
+        private ILabel label;
+        public LabelStuff(String LabelName) { label = DYMO.Label.Framework.Label.Open(LabelName); }
+        public LabelStuff() { }
+        private void SetBarcode(String BarcodeData) { label.SetObjectText("BARCODE", BarcodeData); }
+        private void SetInfoData(String InfoData) { label.SetObjectText("INFOTXT", InfoData); }
+        private String GetBarcode() { return label.GetObjectText("BARCODE"); }
+        private String GetLabel() { return label.GetObjectText("INFOTXT"); }
+        private void PrintLabel() { label.Print("DYMO LabelWriter 400"); }
     }
 }
